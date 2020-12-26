99e1d3436a1656f7c97141d46c69cd3440eeb738
Leo Cheung <leozhang621@gmail.com>
2020-12-26 18:34:04 +0800

    add static page content

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_page/home'
   get 'static_page/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
