c2cba48491a8e808e45095def09264043fc86fd9
Leo Cheung <leozhang621@gmail.com>
2020-12-29 11:24:11 +0800

    remove test routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
   get 'static_page/home'
   get 'static_page/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
