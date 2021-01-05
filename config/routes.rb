63dd9cbb55661803d9be2dc11fe83bbd257c3691
Leo Cheung <leozhang621@gmail.com>
2021-01-05 18:26:29 +0800

    add login page

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_page#about'
   get '/contact', to: 'static_page#contact'
   get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
