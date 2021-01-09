0ba168e83383a053721617c8506dd01f74158e98
Leo Cheung <leozhang621@gmail.com>
2021-01-09 18:23:07 +0800

    add remember_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  attr_accessor :remember_token
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
@@ -9,9 +11,18 @@ class User < ApplicationRecord
 
   before_save { email.downcase! }
 
-  def User.digest(str)
+  def self.digest(str)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(str, cost: cost)
+  end
+
+  def self.new_toke
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_toke
+    update_attribute :remember_digest, User.digest(remember_token)
   end
 
 end
