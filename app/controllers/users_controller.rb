7ab80605795e0a7133bc43a45486d0b329690bf3
Leo Cheung <leozhang621@gmail.com>
2021-01-12 10:22:22 +0800

    implement advanture login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.saveœ
       log_in @user
       flash[:success] = 'Welcome to the mini-twitter'
       redirect_to @user
@@ -21,8 +21,9 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
+∂
